8d81e8ab0ca6526f9b5aaa7c105f69fa9bf56796
Ash Laidlaw and brendan grubb <brendan@>
2017-01-24 13:21:44 -0800

    add Movies objects and showtime arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 //BACK-END
 
-var movies = function Movie(name, rating, run) {
-  this.name = name;
-  this.rating = rating;
-  this.run = run;
+var movieSelector = function() {
+
 };
 
-// var manCalledOve = new Movie("A Man Called Ove", "PG-13", "1st run");
-// var rogueOne = new Movie("Rogue One: A Star Wars Story", "PG-13", "1st run");
-// var drStrange = new Movie("Moonlight", "R", "1st run");
-// var arrival = new Movie("Arrival", "PG-13", "2nd run");
-// var bladeRunner = new Movie("Blade Runner", "R", "2nd run");
-// var selectMovie = [manCalledOve, rogueOne, drStrange, arrival, bladeRunner];
+function Movie(name, rating, run, time) {
+  this.movieName = name;
+  this.rating = rating;
+  this.run = run;
+  this.time = [];
+};
+
+var manCalledOve = new Movie("A Man Called Ove", "PG-13", "1st run", ["3:00pm", "5:00pm", "7:30pm"]);
+var rogueOne = new Movie("Rogue One: A Star Wars Story", "PG-13", "1st run", ["11:00am", "2:30pm", "4:30pm", "9:00pm"]);
+var moonlight = new Movie("Moonlight", "R", "1st run", ["5:00pm", "8:30pm"]);
+var arrival = new Movie("Arrival", "PG-13", "2nd run", ["11:30am", "3:30pm", "6:00pm"]);
+var bladeRunner = new Movie("Blade Runner", "R", "2nd run", ["8:00pm", "10:00pm"]);
+
 
 // selectMovie.forEach(movie) {
 //
@@ -24,7 +29,7 @@ $(function() {
   $("#movie-selection").change(function() {
     $("#movie-times").empty();
 
-    var movieList = ["ove", "rogue", "strange", "arrival", "bladerunner"];
+    var movieList = ["ove", "rogue", "moonlight", "arrival", "bladerunner"];
 
     for (var i = 0; i < movieList.length; i++) {
       if ($(this).val() === movieList[i]) {
